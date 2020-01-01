a15cee2fa3a5338d1b2a727ec96c5a879bb0dcee
Vatcharin Kongsakul <vatcharinkongsakul@Vatcharins-MacBook-Pro.local>
2020-01-02 00:30:45 +0700

    User Input

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 
- int main()
- {
-     std::cout << "You are a secret agent breaking into a secure server room...";
-     std::cout << std::endl;
-     std::cout << "Enter the correct code to continue...";
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue..." << std::endl;
 
-     int a = 4;
-     int b = 3;
-     int c = 2;
+    // Declare 3 number code
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
 
-     int sum = a + b + c;
-     int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-     std::cout << std::endl;
-     std::cout << sum << std::endl;
-     std::cout << product << std::endl;
+    // Print CodeSum and CodeProduct to the terminal
+    std::cout << std::endl;
+    std::cout << CodeSum << std::endl;
+    std::cout << CodeProduct << std::endl;
 
-     return 0;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    return 0;
  }
